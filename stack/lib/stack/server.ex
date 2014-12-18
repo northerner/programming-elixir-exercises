8b0acb1b830a7894b6aa3313e85e985c89b1e87f
NAME <EMAIL>
2014-12-18 21:55:45 +0000

    add cast to stack

--- stack/lib/stack/server.ex
+++ stack/lib/stack/server.ex
@@ -4,4 +4,8 @@ defmodule Stack.Server do
   def handle_call(:pop, _from, [h|t]) do
     { :reply, h, t } 
   end
+
+  def handle_cast({:push, new_value}, current_list) do
+    { :noreply, [new_value | current_list] } 
+  end
 end
