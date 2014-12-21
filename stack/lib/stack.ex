14398815ddfa802c849998106b35db872a6d270c
NAME <EMAIL>
2014-12-21 16:47:38 +0000

    add supervisors

--- stack/lib/stack.ex
+++ stack/lib/stack.ex
@@ -1,2 +1,7 @@
 defmodule Stack do
+  use Application
+
+  def start(_type, _args) do
+    {:ok, _id} = Stack.Supervisor.start_link(["a", "b", "c"])
+  end
 end
--- stack/lib/stack/server.ex
+++ stack/lib/stack/server.ex
@@ -1,11 +1,37 @@
 defmodule Stack.Server do
   use GenServer
 
+  #####
+  # External API
+
+  def start_link(stack) do
+    GenServer.start_link(__MODULE__, stack, name: __MODULE__)
+  end
+
+  def pop do
+    GenServer.call __MODULE__, :pop
+  end
+
+  def push(new_value) do
+    GenServer.cast __MODULE__, {:push, new_value}
+  end
+
+  ###
+  # GenServer implementation
   def handle_call(:pop, _from, [h|t]) do
     { :reply, h, t } 
   end
 
   def handle_cast({:push, new_value}, current_list) do
     { :noreply, [new_value | current_list] } 
+  end
+
+  def init(stash_pid) do
+    current_stack = Stack.Stash.get_value stash_pid
+    { :ok, {:current_stack, stash_pid} }
+  end
+
+  def terminate(_reason, {current_stack, stash_pid}) do
+    Stack.Stash.save_value stash_pid, current_stack
   end
 end
